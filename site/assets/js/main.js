30ec80f99fef45c2183c7c9bbecc1a0888fe306c
Felipe De Andrade <felipedeandrademendonca@gmail.com>
2021-07-04 14:06:24 -0300

    Implementação de efeitos visuais e do highlight.js, animação do input de pesquisa feito

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,7 +1,7 @@
 const botaoHamburguer = document.querySelector("[data-hamburguer]");
 const mobileMenu = document.querySelector("[data-mobileMenu]");
 
-botaoHamburguer.addEventListener("click", function() {
+botaoHamburguer.addEventListener("click", () => {
     document.documentElement.classList.toggle('menuLateral__ativo');
     mobileMenu.classList.toggle("mobileMenu--show");
 });
@@ -13,4 +13,29 @@ document.documentElement.onclick = function(event) {
     };
 };
 
-const botaoLupa = document.querySelector("[data-lupa]")
+const botaoLupa = document.querySelector("[data-lupa]");
+
+botaoLupa.addEventListener("click", () => {
+    document.querySelector('[data-pesquisa]').classList.toggle('entrada__pesquisa--ativa');
+    document.querySelector('[data-logo]').classList.toggle('logo--ativo');
+});
+
+
+const areaDoCodigo = document.querySelector('[data-codeWrapper]');
+const linguagem = document.querySelector('[data-codeLinguagem]');
+const botao = document.querySelector('[data-botaoPreview]');
+
+function mudaLinguagem() {
+    const codigo = areaDoCodigo.querySelector('[data-code]');
+    areaDoCodigo.innerHTML = `<code class="card__codigo hljs language-${linguagem.value}" data-code contenteditable="true"></code>`
+    areaDoCodigo.firstChild.innerText = codigo.innerText
+}
+
+linguagem.addEventListener('change', () => {
+    mudaLinguagem()
+})
+
+botao.addEventListener('click', () => {
+    const codigo = areaDoCodigo.querySelector('[data-code]')
+    hljs.highlightBlock(codigo);
+})
